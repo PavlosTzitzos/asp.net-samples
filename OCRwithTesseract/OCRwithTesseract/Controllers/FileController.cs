693d5ea6f00f0b70e4c5cd7de4e660a78eba396e
admin <admin@DESKTOP-TU7I2AQ>
2023-08-03 16:15:45 +0300

    new projects about OCR

---
+++ OCRwithTesseract/OCRwithTesseract/Controllers/FileController.cs
@@ -0,0 +1,97 @@
+﻿using IronOcr;
+using OCRwithTesseract.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using static OCRwithTesseract.Models.UploadFile;
+using static System.Net.WebRequestMethods;
+
+namespace OCRwithTesseract.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly UploadFileDBContext db = new UploadFileDBContext("Name=OCRwithTesseract");
+
+        // GET: File
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //one file upload :
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                string _FileName = Path.GetFileName(file.FileName);
+                string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                file.SaveAs(_path);
+                //
+                var Ocr = new IronTesseract(); // nothing to configure
+                Ocr.Language = OcrLanguage.English;
+                Ocr.Configuration.TesseractVersion = TesseractVersion.Tesseract5;
+                string ExtractedText;
+                using (var Input = new OcrInput())
+                {
+                    Input.AddImage(_path);
+                    Input.DeNoise();
+                    var Result = Ocr.Read(Input);
+                    ExtractedText = Result.Text;
+                }
+                //
+                // Save the file information to the database or session, if needed.
+                //Saving the data in the DB
+                byte[] fileContent;
+                using (var binaryReader = new BinaryReader(file.InputStream))
+                {
+                    fileContent = binaryReader.ReadBytes(file.ContentLength);
+                }
+                var model = new UploadFile
+                {
+                    FileName = file.FileName,
+                    ContentType = file.ContentType,
+                    Content = fileContent,
+                    FilePath = _path,
+                    ExtractedText = ExtractedText
+                };
+                // Save the uploadedFile object to the database using your DbContext
+                db.UploadFiles.Add(model);
+                db.SaveChanges();
+                
+                return RedirectToAction("Index");
+            }
+            ViewBag.Message = "File upload failed!!";
+            // Handle the case when no file is uploaded
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UploadFile UploadedFile = db.UploadFiles.Find(id);
+            if (UploadedFile == null)
+            {
+                return HttpNotFound();
+            }
+            db.UploadFiles.Remove(UploadedFile);
+            db.SaveChanges();
+            return RedirectToAction("ViewOldUpload");
+        }
+
+        public ActionResult ViewOldUpload()
+        {
+            return View(db.UploadFiles.ToList());
+        }
+    }
+}
---
+++ OCRwithTesseract/OCRwithTesseract/Models/UploadFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+
+namespace OCRwithTesseract.Models
+{
+    public class UploadFile
+    {
+        [DisplayName("ID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FileId { get; set; }
+
+        [DisplayName("File Name")]
+        public string FileName { get; set; }
+
+        [DisplayName("File Type")]
+        public string ContentType { get; set; }
+
+        [DisplayName("File Content")]
+        public byte[] Content { get; set; }
+
+        [DisplayName("Path")]
+        public string FilePath { get; set; }
+
+        [DisplayName("Extracted Text")]
+        public string ExtractedText { get; set; }
+
+        public class UploadFileDBContext : DbContext
+        {
+            public UploadFileDBContext(string nameOrConnectionString) : base(nameOrConnectionString) { }
+            public DbSet<UploadFile> UploadFiles { get; set; }
+        }
+    }
+}
